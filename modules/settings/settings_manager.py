5cfa008d50ed6395c6c3ccfdd794c47a6f08df7f
nech14 <nech14@mail.ru>
2024-09-08 18:09:34 +0800

    fix

--- modules/settings/settings_manager.py
+++ modules/settings/settings_manager.py
@@ -1,4 +1,3 @@
-
 from modules.settings.settings_base import Settings
 import os
 import json
@@ -11,40 +10,28 @@ import json
 class Settings_manager:
     __file_name = "settings.json"
     __settings: Settings = None
-    __text_encoding: str = 'windows-1251'
-
+    __text_encoding: str = 'utf-8'
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(Settings_manager, cls).__new__(cls)
         return cls.instance
 
-
     def __init__(self) -> None:
         if self.__settings is None:
             self.__settings = self.__default_setting()
 
 
-    def read_settings(self, file_name: str, text_encoding: str=None):
-        if not text_encoding is None and isinstance(text_encoding, str):
-            self.__text_encoding = text_encoding
-
-        if isinstance(file_name, str) and file_name != "":
-           self.__file_name = file_name
-
-
-    def go_to_norm_encod(self, s:str):
-        if not isinstance(s, str):
-            return s
-        return s.encode(self.__text_encoding).decode('utf-8')
-
-
     """
     Открыть и загрузить настройки
     """
-    def open(self, file_name: str = "", file_path: str = ""):
+
+    def open(self, file_name: str = "", file_path: str = "", text_encoding: str = ""):
         if not isinstance(file_name, str) and not isinstance(file_path, str):
             raise TypeError("Некорректно переданы параметры!")
+
+        if isinstance(text_encoding, str) and text_encoding != "":
+            self.__text_encoding = text_encoding
 
         if file_name != "":
             self.__file_name = file_name
@@ -55,7 +42,7 @@ class Settings_manager:
             else:
                 full_name = f".{os.sep}{os.curdir}{os.sep}data{os.sep}{self.__file_name}"
 
-            stream = open(full_name)
+            stream = open(full_name, encoding=self.__text_encoding)
             data = json.load(stream)
 
             # Список полей от типа назначения
@@ -65,7 +52,7 @@ class Settings_manager:
             for field in fields:
                 keys = list(filter(lambda x: x == field, data.keys()))
                 if len(keys) != 0:
-                    value = self.go_to_norm_encod(data[field])
+                    value = data[field]
                     # Если обычное свойство - заполняем.
                     if not isinstance(value, list) and not isinstance(value, dict) and value != "":
                         setattr(self.__settings, field, value)
@@ -75,18 +62,18 @@ class Settings_manager:
             self.__settings = self.__default_setting()
             return False
 
-
     """
     Загруженные настройки
     """
+
     @property
     def settings(self):
         return self.__settings
 
-
     """
     Набор настроек по умолчанию
     """
+
     def __default_setting(self):
         data = Settings()
         data.inn = "380080920202"
