0e15c9073b823b82197f455fbfc0e79fa0ce9fa3
nech14 <nech14@mail.ru>
2024-09-26 22:47:39 +0800

    update abstract model

--- modules/models/abstract_model.py
+++ modules/models/abstract_model.py
@@ -58,3 +58,48 @@ class abstract_model(ABC):
 
     def __eq__(self, value: object) -> bool:
         return self.set_compare_mode(value)
+
+
+    """
+    Преобразование класса в словарь
+    """
+
+    def get_dict(self):
+        # Рекурсивная функция для преобразования атрибутов в словарь
+        def recursive_to_dict(value):
+            if hasattr(value, '__dict__'):  # Проверяем, является ли объект пользовательским классом
+                return {self._normalize_key(key): recursive_to_dict(getattr(value, key))
+                        for key in vars(value)}
+            elif isinstance(value, list):  # Если это список, рекурсивно обрабатываем каждый элемент
+                return [recursive_to_dict(item) for item in value]
+            elif isinstance(value, dict):  # Если это словарь, обрабатываем ключи и значения
+                return {k: recursive_to_dict(v) for k, v in value.items()}
+            else:
+                return value  # Для примитивных типов возвращаем значение как есть
+
+        # Преобразуем атрибуты текущего объекта в словарь
+        return {self._normalize_key(key): recursive_to_dict(value) for key, value in vars(self).items()}
+
+    def _normalize_key(self, key):
+        # Убираем имя класса из "name mangled" атрибутов, если они есть
+        if key.startswith('_'):
+            parts = key.split('__', 1)  # Разбиваем по двойному подчёркиванию
+            if len(parts) > 1:
+                return parts[1]  # Возвращаем только часть без имени класса
+        return key  # Если не приватный атрибут, возвращаем как есть
+
+
+    def __str__(self):
+        # Для удобства вывода преобразуем словарь в строку
+        return str(self.get_dict())
+
+    """
+    Проверка реализован ли метод у класса, а не у родителя
+    """
+
+    def is_method_defined_in_child(self, method_name):
+        # Получаем метод из текущего класса
+        method = getattr(self, method_name, None)
+        # Проверяем, что метод существует и он определён в текущем классе
+        return method is not None and method.__qualname__.startswith(self.__class__.__name__)
+
