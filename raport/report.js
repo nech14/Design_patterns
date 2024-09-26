e9301bdd817f71ed7fadcf00e780e18326f526ac
nech14 <nech14@mail.ru>
2024-09-27 02:48:48 +0800

    create raports

---
+++ raport/report.js
@@ -0,0 +1,59 @@
+{
+   "nomenclature_model": [
+      {
+         "name": "Пшеничная мука",
+         "group": {
+            "name": "Сырье"
+         },
+         "range": {
+            "name": "гр",
+            "conversion_factor": 1,
+            "base_unit_measurement": null
+         }
+      },
+      {
+         "name": "Сахар",
+         "group": {
+            "name": "Сырье"
+         },
+         "range": {
+            "name": "гр",
+            "conversion_factor": 1,
+            "base_unit_measurement": null
+         }
+      },
+      {
+         "name": "Сливочное масло",
+         "group": {
+            "name": "Сырье"
+         },
+         "range": {
+            "name": "гр",
+            "conversion_factor": 1,
+            "base_unit_measurement": null
+         }
+      },
+      {
+         "name": "Яйца",
+         "group": {
+            "name": "Сырье"
+         },
+         "range": {
+            "name": "шт",
+            "conversion_factor": 1,
+            "base_unit_measurement": null
+         }
+      },
+      {
+         "name": "Ванилин(щепотка)",
+         "group": {
+            "name": "Сырье"
+         },
+         "range": {
+            "name": "гр",
+            "conversion_factor": 1,
+            "base_unit_measurement": null
+         }
+      }
+   ]
+}
