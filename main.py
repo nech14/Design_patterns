cb31dacd8450bd210a040b00fecebd1feed1f878
nech14 <nech14@mail.ru>
2024-10-12 23:34:09 +0800

    update api

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-
 import connexion
+from flask import request
 
+from modules.Dto.filter_manager import Filter_manager
+from modules.Dto.filter_objects import filter_objects
+from modules.Dto.filtration_type import filtration_type
+from modules.data_key import data_key
 from modules.data_reposity import data_reposity
+from modules.prototype.prototype import prototype
 from modules.settings.settings_manager import Settings_manager
 from modules.start_service import start_service
 from modules.reports.format_reporting import format_reporting
@@ -18,10 +23,28 @@ start.create()
 
 report_manager = Report_manager()
 
+f_m = Filter_manager()
+f_m.update_filter_property()
+f_m.update_filter()
+
 
 @app.route("/api/reports/formats", methods=["GET"])
 def formats():
     return [{"name": item.name, "value": item.value} for item in format_reporting]
+
+@app.route("/api/filter/formats", methods=["GET"])
+def filter_format():
+    return [{"name": item.name} for item in filter_objects]
+
+@app.route("/api/filter/type", methods=["GET"])
+def filter_type():
+    return [{"name": item.name, "value": item.value} for item in filtration_type]
+
+
+@app.route("/api/filter/base/property", methods=["GET"])
+def filter_base_property():
+    return f_m.filter_property
+
 
 @app.route("/api/reports/range/<format>", methods=["GET"])
 def reports_range(format: str):
@@ -65,6 +88,20 @@ def reports_receipt(format: str):
     return report.result
 
 
+@app.route("/api/filter/<string:domain>", methods=["POST"])
+def filter_data(domain):
+    json_data = request.json
+
+    f_m.update_filter_property()
+    f_m.update_filter()
+    f_m.update_property_filter(**json_data)
+    data = reposity.data[list(data_key)[int(domain)].value]
+    p = prototype()
+
+    new_data = p.create(data, f_m.filter, f_m.filter_property).data
+    return f"{new_data}"
+
 if __name__ == '__main__':
     app.add_api("swagger.yaml")
     app.run(port = 8080)
+
