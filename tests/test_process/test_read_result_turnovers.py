c04980a005f23a63fe24b69aac1f17ab34c394a7
nech14 <nech14@mail.ru>
2024-10-30 23:45:36 +0800

    create|update tests

---
+++ tests/test_process/test_read_result_turnovers.py
@@ -0,0 +1,39 @@
+import os
+import unittest
+from pathlib import Path
+
+from modules.models.warehouse_turnover_model import warehouse_turnover_model
+from modules.process.models.read_result_turnovers import read_result_turnovers
+from modules.process.models.save_result_turnovers import save_result_turnovers
+from modules.process.modified_list import modified_list
+
+
+class Test_read_result_turnovers(unittest.TestCase):
+
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    file_path = os.path.join(root_path, "result", "result_process_test.json")
+    file_path = Path(file_path)
+
+    def test_read_result_turnovers(self):
+
+        data_list = modified_list()
+        data_list.file_path = f"{self.file_path}"
+
+        for i in range(10):
+            item_warehouse_turnover = warehouse_turnover_model.create_default()
+            data_list.append(item_warehouse_turnover)
+
+        result = save_result_turnovers.start_process(data_list)
+
+        self.assertEqual(
+            result,
+            True
+        )
+
+        data = read_result_turnovers.start_process(data_list)
+
+        self.assertEqual(
+            data_list,
+            data
+        )
+
---
+++ tests/test_process/test_save_result_turnovers.py
@@ -0,0 +1,44 @@
+import json
+import os
+import unittest
+from pathlib import Path
+
+from modules.models.abstract_model import abstract_model
+from modules.models.warehouse_turnover_model import warehouse_turnover_model
+from modules.process.models.save_result_turnovers import save_result_turnovers
+from modules.process.modified_list import modified_list
+
+
+class Test_save_result_turnovers(unittest.TestCase):
+    root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+    file_path = os.path.join(root_path, "result", "result_process_test.json")
+    file_path = Path(file_path)
+
+    def test_save_result_turnovers(self):
+
+        data_list = modified_list()
+        data_list.file_path = f"{self.file_path}"
+
+        for i in range(10):
+            item_warehouse_turnover = warehouse_turnover_model.create_default()
+            data_list.append(item_warehouse_turnover)
+
+
+        result = save_result_turnovers.start_process(data_list)
+
+        self.assertEqual(
+            result, True
+        )
+
+        self.assertEqual(
+            self.file_path.is_file(),
+            True
+        )
+
+        with self.file_path.open("r") as file:
+            data = json.load(file)
+
+        for item, new_item in zip(data_list, data):
+            item: abstract_model
+            self.assertEqual(item.get_dict(), new_item)
+
