3a3d0c9f9a907aade9827d9b89599b92ab4635d1
nech14 <nech14@mail.ru>
2024-11-08 22:15:41 +0800

    fix

--- modules/Enums/data_key.py
+++ modules/Enums/data_key.py
@@ -2,9 +2,9 @@ from enum import Enum
 
 
 class data_key(Enum):
-    nomenclature_model = "nomenclature",
-    nomenclature_group_model = "group",
-    range_model = "range",
-    receipt_model = "receipt",
-    warehouse_model = "warehouse",
+    nomenclature_model = "nomenclature"
+    nomenclature_group_model = "group"
+    range_model = "range"
+    receipt_model = "receipt"
+    warehouse_model = "warehouse"
     warehouse_transaction_model = "warehouse_transaction"
