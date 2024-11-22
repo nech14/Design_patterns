f2a9e1897c4100ee2920c6f207bfc2c55278d843
nech14 <nech14@mail.ru>
2024-11-22 11:51:57 +0800

    curls

---
+++ curl/8000.bat
@@ -0,0 +1,6 @@
+
+curl -X POST "http://localhost:8000/api/block_period/2024-12-01"
+
+curl -X GET "http://localhost:8000/api/block_period/save"
+
+curl -X GET "http://localhost:8000/api/tbs/2024-11-01/2024-11-30/TestWarehouse"
